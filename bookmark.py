77c76cfad9ce82908067b89ecdd23fa74d1cf7cb
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-11-16 15:37:35 +0100

    bookmark: add Option class

--- bookmark.py
+++ bookmark.py
@@ -4,6 +4,20 @@ from commands import (
 )
 
 
+class Option:
+    def __init__(self, option, command, extra=None):
+        self.option = option
+        self.command = command
+        self.extra = extra
+
+    def run(self):
+        prep = self.extra() if self.extra else None
+        output = self.command.execute(prep) if prep else self.command.execute()
+        print(output)
+
+    def __str__(self):
+        return self.option
+
 if __name__ == "__main__":
    initialize_db().execute()
    print("Welcome")
